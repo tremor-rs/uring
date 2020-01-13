419ac43af7c62842699855ff2ca36f70d9d61c61
Heinz Gies <hgies@wayfair.com>
2020-01-13 16:21:21 +0100

    First crack at interceptors for protocols and services

--- protocol-driver/src/lib.rs
+++ protocol-driver/src/lib.rs
@@ -1,10 +1,28 @@
+// Copyright 2018-2020, Wayfair GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 #![recursion_limit = "1024"]
+
+pub mod interceptor;
+
 use futures::channel::mpsc::{channel, Receiver, SendError, Sender};
 use futures::{select, SinkExt, StreamExt};
 use serde_derive::{Deserialize, Serialize};
 use std::{collections::HashMap, hash::Hash};
+pub use uring_common::{RequestId, ServiceId};
 
 pub type CustomProtocol = String;
+
 #[derive(Deserialize, Serialize, PartialEq, Eq, Hash)]
 pub enum Protocol {
     Connect,
@@ -114,22 +132,23 @@ pub type HandlerOutboundData = DriverOutboundData;
 pub struct HandlerInboundMessage {
     pub data: Vec<u8>,
     pub outbound_channel: HandlerOutboundChannelSender,
-    pub id: MessageId,
+    pub service_id: Option<ServiceId>,
+    pub id: RequestId,
 }
 
 pub struct HandlerOutboundMessage {
     pub data: HandlerOutboundData,
-    pub id: MessageId,
+    pub id: RequestId,
 }
 
 impl HandlerOutboundMessage {
-    pub fn ok(id: MessageId, data: Vec<u8>) -> Self {
+    pub fn ok(id: RequestId, data: Vec<u8>) -> Self {
         Self {
             id,
             data: DriverOutboundData::Ok(data),
         }
     }
-    pub fn error<T>(id: MessageId, error: DriverErrorType, message: T) -> Self
+    pub fn error<T>(id: RequestId, error: DriverErrorType, message: T) -> Self
     where
         T: ToString,
     {
@@ -150,14 +169,16 @@ pub type HandlerInboundChannelSender = Sender<HandlerInboundMessage>;
 
 pub struct Driver {
     transport_rx: DriverInboundChannelReceiver,
-    transport_tx: DriverInboundChannelSender,
+    pub transport_tx: DriverInboundChannelSender,
 
     handler_rx: HandlerOutboundChannelReceiver,
     handler_tx: HandlerOutboundChannelSender,
 
-    pending: HashMap<MessageId, DriverOutboundChannelSender>,
+    pending: HashMap<RequestId, (MessageId, DriverOutboundChannelSender)>,
     clients: HashMap<ClientId, ClientConnection>,
     protocol_handlers: HashMap<CustomProtocol, HandlerInboundChannelSender>,
+
+    next_rid: u64,
 }
 
 impl Default for Driver {
@@ -172,6 +193,7 @@ impl Default for Driver {
             pending: HashMap::new(),
             clients: HashMap::new(),
             protocol_handlers: HashMap::new(),
+            next_rid: 0,
         }
     }
 }
@@ -181,133 +203,178 @@ impl Driver {
         loop {
             select! {
                 msg = self.transport_rx.next() => {
-                    let msg = if let Some(msg) = msg {
-                        msg
+                    if let Some(msg) = msg {
+                        self.inbound_handler(msg).await?;
                     } else {
                         // ARGH! errro
                         break;
                     };
-                    let DriverInboundMessage{data, id, mut outbound_channel} = msg;
-
-                    let client = self.clients.entry(id.client).or_default();
-                    let keep_client = match (&client.protocol, &data) {
-                        // When we're in connect
-                        (Protocol::Connect, DriverInboundData::Connect(protos)) => {
-                            //TODO: Validate protocols
-                            client.protocol = Protocol::None;
-                            client.enabled_protocols = protos.clone();
-                            outbound_channel.send(DriverOutboundMessage::ok(id, vec![])).await.is_ok()
-                        }
-                        (Protocol::Connect, _) => {
-                            outbound_channel.send(DriverOutboundMessage::error(
-                                id,
-                                DriverErrorType::InvalidRequest,
-                                "Can not call Connect twice"
-                            )).await.is_ok()
-                        }
-                        // When we've not selected a protocol
-                        (Protocol::Custom(_), DriverInboundData::As(proto, data)) |
-                        (Protocol::None, DriverInboundData::As(proto, data)) => {
-                            if let Some(ref mut handler) = self.protocol_handlers.get_mut(proto) {
-                                self.pending.insert(id, outbound_channel);
-                                let msg = HandlerInboundMessage {
-                                    id,
-                                    data: data.clone(),
-                                    outbound_channel: self.handler_tx.clone(),
-                                };
-                                handler.send(msg).await?;
-                                true
-                            } else {
-                                outbound_channel.send(DriverOutboundMessage::error(
-                                    id,
-                                       DriverErrorType::BadProtocol,
-                                       format!("Invalid protocol {}", proto)
-                                )).await.is_ok()
-                            }
-                        }
-                        (Protocol::None , DriverInboundData::Select(proto)) |
-                        (Protocol::Custom(_), DriverInboundData::Select(proto)) => {
-                            if client.enabled_protocols.contains(proto) {
-                                client.protocol = Protocol::Custom(proto.clone());
-                                outbound_channel.send(DriverOutboundMessage::ok(id, vec![])).await.is_ok()
-                            } else {
-                                outbound_channel.send(DriverOutboundMessage::error(
-                                    id,
-                                    DriverErrorType::BadProtocol,
-                                    format!("Invalid protocol {}", proto)
-                                )).await.is_ok()
-                            }
-                        }
-                        (Protocol::None, _) => {
-                            outbound_channel.send(DriverOutboundMessage::error(
-                                id,
-                                DriverErrorType::InvalidRequest,
-                                "No protocol specified"
-                            )).await.is_ok()
-                        }
-                        // we have a default
-                        (Protocol::Custom(_), DriverInboundData::Connect(_)) => {
-                            outbound_channel.send(DriverOutboundMessage::error(
-                                id,
-                                DriverErrorType::InvalidRequest,
-                                "Can not call Connect twice"
-
-                            )).await.is_ok()
-                        }
-                        (Protocol::Custom(proto), DriverInboundData::Message(data)) => {
-                            if !client.enabled_protocols.contains(&proto) {
-                                outbound_channel.send(DriverOutboundMessage::error(
-                                    id,
-                                    DriverErrorType::BadProtocol,
-                                    format!("Protocol {} is not enabled.", proto)
-                                )).await.is_ok()
-                            } else  if let Some(ref mut handler) = self.protocol_handlers.get_mut(proto) {
-                                let msg = HandlerInboundMessage {
-                                    id,
-                                    data: data.clone(),
-                                    outbound_channel: self.handler_tx.clone(),
-                                };
-                                handler.send(msg).await?;
-                                true
-                            } else {
-                                outbound_channel.send(DriverOutboundMessage::error(
-                                    id,
-                                    DriverErrorType::BadProtocol,
-                                    format!("Protocol {} is not known.", proto)
-                                )).await.is_ok()
-                            }
-                        }
-                        (_, DriverInboundData::Disconnect) => {
-                            false
-                        }
-                    };
-                    if !keep_client {
-                        self.clients.remove(&id.client);
-
-                    }
-
                 },
                 msg = self.handler_rx.next() => {
-                    let msg = if let Some(msg) = msg {
-                        msg
+                    if let Some(msg) = msg {
+                        self.outbound_handler(msg).await?;
                     } else {
                         // ARGH! errro
                         break;
                     };
-                    let HandlerOutboundMessage {
-                        data,
-                        id,
-                    } = msg;
-                    if let Some(mut transport) = self.pending.remove(&id) {
-                        let msg = DriverOutboundMessage{
-                            id, data
-                        };
-                        transport.send(msg).await?;
-                    };
+
                 },
             };
         }
         Ok(())
+    }
+
+    async fn outbound_handler(&mut self, msg: HandlerOutboundMessage) -> Result<(), SendError> {
+        let HandlerOutboundMessage { data, id } = msg;
+        if let Some((id, mut transport)) = self.pending.remove(&id) {
+            let msg = DriverOutboundMessage { id, data };
+            transport.send(msg).await?;
+        };
+        Ok(())
+    }
+
+    #[allow(mutable_transmutes)]
+    async fn inbound_handler(&mut self, msg: DriverInboundMessage) -> Result<(), SendError> {
+        let DriverInboundMessage {
+            data,
+            id,
+            mut outbound_channel,
+        } = msg;
+
+        let client = self.clients.entry(id.client).or_default();
+        let keep_client = match (&client.protocol, &data) {
+            // When we're in connect
+            (Protocol::Connect, DriverInboundData::Connect(protos)) => {
+                //TODO: Validate protocols
+                client.protocol = Protocol::None;
+                client.enabled_protocols = protos.clone();
+                outbound_channel
+                    .send(DriverOutboundMessage::ok(id, vec![]))
+                    .await
+                    .is_ok()
+            }
+            (Protocol::Connect, _) => outbound_channel
+                .send(DriverOutboundMessage::error(
+                    id,
+                    DriverErrorType::InvalidRequest,
+                    "Can not call Connect twice",
+                ))
+                .await
+                .is_ok(),
+            // When we've not selected a protocol
+            (Protocol::Custom(_), DriverInboundData::As(proto, data))
+            | (Protocol::None, DriverInboundData::As(proto, data)) => {
+                if let Some(handler) = self.protocol_handlers.get(proto) {
+                    // Rust does not recognize that the mutable borrow of handler never will relocate
+                    // when we register pending so we got to transmute the hell out of it.
+                    let handler: &mut Sender<_> = unsafe { std::mem::transmute(handler) };
+                    let rid = self.register_pending(id, outbound_channel);
+                    let msg = HandlerInboundMessage {
+                        id: rid,
+                        data: data.clone(),
+                        outbound_channel: self.handler_tx.clone(),
+                        service_id: None,
+                    };
+                    handler.send(msg).await?;
+                    true
+                } else {
+                    outbound_channel
+                        .send(DriverOutboundMessage::error(
+                            id,
+                            DriverErrorType::BadProtocol,
+                            format!("Invalid protocol {}", proto),
+                        ))
+                        .await
+                        .is_ok()
+                }
+            }
+            (Protocol::None, DriverInboundData::Select(proto))
+            | (Protocol::Custom(_), DriverInboundData::Select(proto)) => {
+                if client.enabled_protocols.contains(proto) {
+                    client.protocol = Protocol::Custom(proto.clone());
+                    outbound_channel
+                        .send(DriverOutboundMessage::ok(id, vec![]))
+                        .await
+                        .is_ok()
+                } else {
+                    outbound_channel
+                        .send(DriverOutboundMessage::error(
+                            id,
+                            DriverErrorType::BadProtocol,
+                            format!("Invalid protocol {}", proto),
+                        ))
+                        .await
+                        .is_ok()
+                }
+            }
+            (Protocol::None, _) => outbound_channel
+                .send(DriverOutboundMessage::error(
+                    id,
+                    DriverErrorType::InvalidRequest,
+                    "No protocol specified",
+                ))
+                .await
+                .is_ok(),
+            // we have a default
+            (Protocol::Custom(_), DriverInboundData::Connect(_)) => outbound_channel
+                .send(DriverOutboundMessage::error(
+                    id,
+                    DriverErrorType::InvalidRequest,
+                    "Can not call Connect twice",
+                ))
+                .await
+                .is_ok(),
+            (Protocol::Custom(proto), DriverInboundData::Message(data)) => {
+                if !client.enabled_protocols.contains(&proto) {
+                    outbound_channel
+                        .send(DriverOutboundMessage::error(
+                            id,
+                            DriverErrorType::BadProtocol,
+                            format!("Protocol {} is not enabled.", proto),
+                        ))
+                        .await
+                        .is_ok()
+                } else if let Some(handler) = self.protocol_handlers.get(proto) {
+                    // Rust does not recognize that the mutable borrow of handler never will relocate
+                    // when we register pending so we got to transmute the hell out of it.
+                    let handler: &mut Sender<_> = unsafe { std::mem::transmute(handler) };
+                    let rid = self.register_pending(id, outbound_channel);
+                    let msg = HandlerInboundMessage {
+                        id: rid,
+                        data: data.clone(),
+                        outbound_channel: self.handler_tx.clone(),
+                        service_id: None,
+                    };
+                    handler.send(msg).await?;
+                    true
+                } else {
+                    outbound_channel
+                        .send(DriverOutboundMessage::error(
+                            id,
+                            DriverErrorType::BadProtocol,
+                            format!("Protocol {} is not known.", proto),
+                        ))
+                        .await
+                        .is_ok()
+                }
+            }
+            (_, DriverInboundData::Disconnect) => false,
+        };
+        if !keep_client {
+            self.clients.remove(&id.client);
+        }
+        Ok(())
+    }
+
+    fn register_pending(
+        &mut self,
+        mid: MessageId,
+        sender: DriverOutboundChannelSender,
+    ) -> RequestId {
+        let rid = RequestId(self.next_rid);
+        self.next_rid += 1;
+        self.pending.insert(rid, (mid, sender));
+        rid
     }
 }
 
--- src/protocol/kv.rs
+++ src/protocol/kv.rs
@@ -1,34 +1,59 @@
-use protocol_driver::{HandlerInboundChannelSender, HandlerInboundChannelReceiver, HandlerInboundMessage, HandlerOutboundMessage, DriverErrorType};
-use futures::channel::mpsc::SendError;
-use futures::{select, SinkExt, StreamExt};
+// Copyright 2018-2020, Wayfair GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::service::kv;
+use async_trait::async_trait;
+use protocol_driver::{interceptor, DriverErrorType, HandlerInboundMessage};
 use serde_derive::{Deserialize, Serialize};
 
-
-struct KVHandler {
-    tx: HandlerInboundChannelSender,
-    rx: HandlerInboundChannelReceiver
-}
-
-
 #[derive(Deserialize, Serialize)]
-enum KVMessage {
-    Get{key: String}
+enum Request {
+    Get {
+        key: String,
+    },
+    Put {
+        key: String,
+        store: String,
+    },
+    Delete {
+        key: String,
+    },
+    Cas {
+        key: String,
+        check: Option<String>,
+        store: String,
+    },
 }
 
-impl KVHandler {
-    pub async fn run_loop(&mut self) -> Result<(), SendError> {
-        while let Some(HandlerInboundMessage{data, mut outbound_channel, id} )= self.rx.next().await {
-            match serde_json::from_slice(&data) {
-                Ok(KVMessage::Get{key}) => {
-                    outbound_channel.send(HandlerOutboundMessage::ok(id, vec![])).await?;
-                }
-                Err(_) => {
-                    outbound_channel.send(HandlerOutboundMessage::error(id, DriverErrorType::BadInput, "Not a valid KV message")).await?;
-                }
+struct Handler {}
 
-            }
-
-        }
-        Ok(())
+#[async_trait]
+impl interceptor::Intercept for Handler {
+    async fn inbound(&mut self, mut msg: HandlerInboundMessage) -> interceptor::Reply {
+        use kv::Event;
+        msg.service_id = Some(kv::ID);
+        msg.data = match serde_json::from_slice(&msg.data) {
+            Ok(Request::Get { key }) => Event::get(key.into_bytes()),
+            Ok(Request::Put { key, store }) => Event::put(key.into_bytes(), store.into_bytes()),
+            Ok(Request::Delete { key }) => Event::delete(key.into_bytes()),
+            Ok(Request::Cas { key, check, store }) => kv::Event::cas(
+                key.into_bytes(),
+                check.map(String::into_bytes),
+                store.into_bytes(),
+            ),
+            Err(_) => return interceptor::Reply::Err(DriverErrorType::BadInput),
+        };
+        interceptor::Reply::Ok(msg)
     }
 }
